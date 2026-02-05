e852fb37cf10c9f2fdf296275a87f8dbdcef4af9
gndc406 <giovanna.v.nolascodecarvalho@wmich.edu>
2026-02-05 00:56:23 -0500

    Add files via upload
    This is my submission for Assignment 2: Learning Bash

---
+++ learning_bash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+greeting="Good luck, you are going to need it," name=Giovanna new_name=bash_template.sh
+echo "$greeting" "$name"
+echo "This script is helping you learn bash"
+mkdir -p /home/gndc/Desktop/bioinformatics/bash_scripts
+mv bash_template.txt /home/gndc/Desktop/bioinformatics/bash_scripts/$new_name
